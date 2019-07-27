eefd35562ececb38563a19d60532b63f001743f6
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2019-07-27 18:34:34 +0300

    Update Task8.java

--- 2_Algorithmization/one_dimension_arrays/Task8.java
+++ 2_Algorithmization/one_dimension_arrays/Task8.java
@@ -9,15 +9,15 @@ public class Task8 {
             System.out.print(x + "  ");
         }
         int min = array[0];              
-        for(int i = 0; i < array.length; i++) {
+        for(int i = 0; i < array.length; i++) {  // ищем минимум в массиве
             if(array[i] < min) min = array[i];
         }
         System.out.println("\nМинимум: " + min);
         int n = 0;
-        for(int x : array) {
+        for(int x : array) {   // подсчитываем число элементов, которые не равны минимуму
             if(x != min) n++;
         }
-        int array2[] = new int[n];
+        int array2[] = new int[n];   // создаем новый массив и заполняем его элементами массива, которые не равны минимуму
         for(int i = 0, j = 0; i < array.length; i++) {
             if(array[i] == min) continue;
             array2[j] = array[i];
