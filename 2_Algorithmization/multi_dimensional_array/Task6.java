da90c0e613bf7de52499ac7951e217f8bdda1b4d
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2019-07-27 19:17:28 +0300

    Create Task6.java

---
+++ 2_Algorithmization/multi_dimensional_array/Task6.java
@@ -0,0 +1,27 @@
+public class Task6 {    
+    public static void main(String[] args) {
+        int S = 6;
+        int array[][] = new int[S][S];
+        for(int i = 0; i < S; i++) {
+            for(int j = 0; j < S; j++) {
+                if(i <= (S/2 - 1)) {
+                    if(j > (S - 1 - i) || j <= (i - 1)) 
+                        array[i][j] = 0;
+                    else array[i][j] = 1;
+                }
+                else {
+                    if(j < (S - 1 - i) || j > i) 
+                        array[i][j] = 0;
+                    else array[i][j] = 1;
+                }
+            }
+        }
+        System.out.println("Матрица: ");
+        for(int x[] : array) {
+            for(int y : x) {
+                System.out.print(y + " ");
+            }
+            System.out.println();
+        }       
+    }
+}
