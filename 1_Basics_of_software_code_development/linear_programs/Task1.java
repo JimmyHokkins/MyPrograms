5d17e86279c21bfc1421c1d0c94f44792a854ccc
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2019-07-27 12:58:24 +0300

    Create Task1.java

---
+++ 1_Basics_of_software_code_development/linear_programs/Task1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Task1 {    
+    public static void main(String[] args) {
+        double a, b, c;
+        System.out.print("Введите числа a, b и c (через пробел): ");
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        c = sc.nextDouble();
+        System.out.println("Значение функции z = ((a - 3) * b / 2) + c = " + (((a - 3) * b / 2) + c));
+    }    
+}
