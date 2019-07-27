7b201f94accdf9146fb1dfa32fcacba2054a9c42
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2019-07-27 17:27:05 +0300

    Create Task1.java

---
+++ 2_Algorithmization/one_dimension_arrays/Task1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import static java.lang.Math.*;
+
+public class Task1 {    
+    public static void main(String[] args) {
+        int[] A = new int[15];
+        for(int i = 0; i < A.length; i++){
+            A[i] = (int)(random() * 15) + 1;
+        }
+        System.out.println("Исходный массив: ");
+        for(int x : A) {
+            System.out.print(x + "  ");
+        }
+        System.out.println();
+        int sum = 0, k;
+        System.out.print("Укажите число К: ");
+        Scanner sc = new Scanner(System.in);
+        k = sc.nextInt();
+        for(int n : A) {
+            if(n % k == 0) sum += n;
+        }
+        System.out.println("Сумма чисел, кратных К: " + sum);
+    }
+}
