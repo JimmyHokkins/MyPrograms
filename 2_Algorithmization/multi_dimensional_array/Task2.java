5882ecc9b35fe3ff23dc7661927d8abeacb4568d
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2019-07-27 19:07:13 +0300

    Create Task2.java

---
+++ 2_Algorithmization/multi_dimensional_array/Task2.java
@@ -0,0 +1,25 @@
+public class Task2 {    
+    public static void main(String[] args) {
+        int S = 5;
+        int array[][] = new int[S][S];
+        for(int i = 0; i < S; i++) {
+            for(int j = 0; j < S; j++) {
+                array[i][j] = (int)(Math.random() * 10);
+            }
+        }        
+        System.out.println("Исходная матрица: ");
+        for(int x[] : array) {
+            for(int y : x) {
+                System.out.print(y + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Элементы, стоящие по диагонали: ");
+        for(int i = 0; i < S; i++) {
+            for(int j = 0; j < S; j++) {
+                if(j == i) System.out.print(array[i][j] + " ");
+            }
+        }         
+        System.out.println();      
+    }
+}
