07564f76be9cb03b6ff525ecfc2e4b3acf047890
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2019-07-27 18:03:26 +0300

    Create Task5.java

---
+++ 2_Algorithmization/one_dimension_arrays/Task5.java
@@ -0,0 +1,17 @@
+public class Task5 {    
+    public static void main(String[] args) {
+        int array[] = new int[10];
+        for(int i = 0; i < array.length; i++) {
+            array[i] = (int)(Math.random() * 21 - 10);
+        }        
+        System.out.print("Исходный массив: ");
+        for(int x : array) {
+            System.out.print(x + "  ");
+        }       
+        System.out.println("\nЧисла в массиве, которые больше своего индекса: ");
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] > i) System.out.println(i + "-й элемент: " + array[i]);
+        }
+        System.out.println();       
+    }
+}
