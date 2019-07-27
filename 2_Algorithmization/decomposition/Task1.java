7737c5605caebe11be6f882d22eed685e7a100d6
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2019-07-27 22:27:57 +0300

    Create Task1.java

---
+++ 2_Algorithmization/decomposition/Task1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Task1 {    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите первое натуральное число: ");
+        int a = sc.nextInt();
+        System.out.print("Введите второе натуральное число: ");
+        int b = sc.nextInt();        
+        System.out.println("Наибольший общий делитель: " + nod(a, b));
+        System.out.println("Наименьшее общее кратное: " + nok(a, b));        
+    }
+    static int nod(int x, int y) {
+        if(x == y) return x;
+        int small, big, divider = 1;
+        small = x < y? x : y;
+        big = x > y? x : y;
+        for(int i = 1; i <= small; i++) {
+            if(small % i == 0 && big % i == 0) divider = i;
+        }
+        return divider;
+    }
+    static int nok(int x, int y) {
+        if(x == y) return x;
+        int small, big; 
+        small = x < y? x : y;
+        big = x > y? x : y;
+        int multiple = big;
+        while(multiple % small != 0 || multiple % big != 0) {
+            multiple += big;
+        }
+        return multiple;
+    }
+}
