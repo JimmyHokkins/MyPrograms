1a141f93c4e57f4fcd3832a1726a417f2ccce721
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2019-07-27 17:00:39 +0300

    Create Task6.java

---
+++ 1_Basics_of_software_code_development/cycles/Task6.java
@@ -0,0 +1,13 @@
+public class Task6 {    
+    public static void main(String[] args) throws java.io.IOException {
+        int ch, ch1;         
+        do {
+            System.out.print("Введите любой символ (0 - для выхода): ");
+            ch = System.in.read();
+            System.out.println("Числовой код данного символа: " + ch);
+            do {
+                ch1 = System.in.read(); // считываем оставшуюся во входном потоке информацию для его очистки
+            } while(ch1 != '\n');   
+        } while(ch != '0');
+    }
+}
