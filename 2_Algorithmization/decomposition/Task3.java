9c361fca56921678bb804d128693d588920fe97d
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2019-07-27 22:30:45 +0300

    Create Task3.java

---
+++ 2_Algorithmization/decomposition/Task3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import static java.lang.Math.*;
+
+public class Task3 {    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите размер стороны правильного шестиугольника: ");
+        int a = sc.nextInt();       
+        System.out.println("Площадь шестиугольника: " + squareSix(a));                
+    }
+    static double squareSix(int x) {        
+        return 6 * sqrt(3)/4 * pow(x, 2);
+    }   
+}
