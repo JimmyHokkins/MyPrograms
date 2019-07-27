19d4edbdd96cb444c2850d8a00eb6e8b339d9d3d
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2019-07-27 17:33:28 +0300

    Create Task2.java

---
+++ 2_Algorithmization/one_dimension_arrays/Task2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import static java.lang.Math.*;
+
+public class Task2 {    
+    public static void main(String[] args) {
+        double array[] = new double[10];
+        for(int i = 0; i < array.length; i++) {
+            array[i] = round((random() * 201 - 100) * 100) / 100.0;     // заполняем массив случайными числами
+        }
+        System.out.print("Исходный массив: ");
+        for(double x : array) {
+            System.out.print(x + "  ");
+        }        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nВведите число Z: ");
+        double z = sc.nextDouble();
+        int count = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] > z) {
+                array[i] = z;
+                count++;
+            }
+        }        
+        System.out.print("Преобразованный массив: ");
+        for(double x : array) {
+            System.out.print(x + "  ");
+        }
+        System.out.println("\nЧисло замен: " + count);       
+    }
+}
