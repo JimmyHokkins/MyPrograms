9549a613f263ce1206ece2574ddc53d7f3b7d37c
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2019-07-27 22:29:13 +0300

    Create Task2.java

---
+++ 2_Algorithmization/decomposition/Task2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Task2 {    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите четыре натуральных числа (через пробел): ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();        
+        System.out.println("Наибольший общий делитель для них: " + nod(a, b, c, d));                
+    }
+    static int nod(int w, int x, int y, int z) {
+        int[] numbs = { w, x, y, z };
+        int small = numbs[0];
+        for(int i = 0; i < numbs.length; i++){
+            if(numbs[i] < small) small = numbs[i];            
+        }
+        int divider = 1;        
+        for(int i = 2; i <= small; i++) {
+            if(w % i == 0 && x % i == 0 && y % i == 0 && z % i == 0) divider = i;
+        }
+        return divider;
+    }   
+}
