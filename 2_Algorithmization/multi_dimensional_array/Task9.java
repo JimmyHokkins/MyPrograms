f31aad74b48877fc853e6b219845d55405881cd3
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2019-07-27 19:32:36 +0300

    Create Task9.java

---
+++ 2_Algorithmization/multi_dimensional_array/Task9.java
@@ -0,0 +1,30 @@
+public class Task9 {    
+    public static void main(String[] args) {
+        int S1 = 6, S2 = 10;
+        int array[][] = new int[S1][S2];
+        for(int i = 0; i < S1; i++) {
+            for(int j = 0; j < S2; j++) {
+                array[i][j] = (int)(Math.random() * 10);
+            }
+        }        
+        System.out.println("Исходная матрица: ");
+        for(int x[] : array) {
+            for(int y : x) {
+                System.out.print(y + " ");
+            }
+            System.out.println();
+        }
+        int sum = 0, sum_max = 0, index = 0;
+        for(int j = 0; j < S2; j++) {
+            for(int i = 0; i < S1; i++) {
+                sum += array[i][j];
+            }
+            if(sum > sum_max) {
+                sum_max = sum;
+                index = j + 1;
+            }
+            sum = 0;
+        }
+        System.out.println("Максимальная сумма элементов равна " + sum_max + " в " + index + "-м столбце.");       
+    }
+}
