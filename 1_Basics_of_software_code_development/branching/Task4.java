774ead3bc01b60e0d296f820bcbcf51a6c4fe691
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2019-07-27 14:01:02 +0300

    Create Task4.java

---
+++ 1_Basics_of_software_code_development/branching/Task4.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Task1 {    
+    public static void main(String[] args) {
+        double x, y, z, s1, s2, s3;  // параметры кирпича
+        double A, B, s;  // параметры отверстия
+        boolean traverse = false;
+        String answer;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите размеры отверстия.");        
+        System.out.print("Размер A: ");
+        A = sc.nextDouble();
+        System.out.print("Размер В: ");
+        B = sc.nextDouble();
+        System.out.println("Введите размеры кирпича.");
+        System.out.print("Размер x: ");
+        x = sc.nextDouble();
+        System.out.print("Размер y: ");
+        y = sc.nextDouble();
+        System.out.print("Размер z: ");
+        z = sc.nextDouble();        
+        s = A * B;  // площадь отверстия
+        s1 = x * y; // площадь 1-ой грани кирпича
+        s2 = x * z; // площадь 2-ой грани кирпича
+        s3 = y * z; // площадь 3-ей грани кирпича        
+        if(s1 < s) {
+            if((x < A && y < B) || (x < B && y < A)) traverse = true;
+        }
+        if(s2 < s) {
+            if((x < A && z < B) || (x < B && z < A)) traverse = true;
+        }
+        if(s3 < s) {
+            if((z < A && y < B) || (z < B && y < A)) traverse = true;
+        }       
+        answer = (traverse == true)? "Кирпич пройдет через отверстие." : "Кирпич не пройдет через отверстие.";
+        System.out.println(answer);
+    }
+}
