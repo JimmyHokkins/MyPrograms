f7ecaf25f71e17498870636fe6c1b8a326dea28c
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2019-07-27 14:22:09 +0300

    Create Task4.java

---
+++ 1_Basics_of_software_code_development/cycles/Task4.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+
+public class Task4 {    
+    public static void main(String[] args) {
+        // т.к. результирующее число очень большое, используем класс BigInteger
+        BigInteger mul = BigInteger.valueOf(1);  // начальное значение произведения
+        BigInteger i = BigInteger.valueOf(1);  // начальное число
+        BigInteger limit = BigInteger.valueOf(200);  // конечное число
+        while(i.compareTo(limit) == -1 || i.compareTo(limit) == 0) {  // пока число i меньше или равно конечного числа
+            mul = mul.multiply(i.multiply(i));   // результирующее произведение квадратов         
+            i = i.add(BigInteger.valueOf(1));  // получаем следующее число
+        }
+        System.out.println("Произведение квадратов первых 200 чисел равна " + mul);
+    }
+}
