14581c57b8213afae319ce44d6c9e0ea4e467b5d
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2019-07-27 19:26:56 +0300

    Create Task8.java

---
+++ 2_Algorithmization/multi_dimensional_array/Task8.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Task8 {    
+    public static void main(String[] args) {
+        int S1 = 5, S2 = 10;
+        int array[][] = new int[S1][S2];
+        for(int i = 0; i < S1; i++) {
+            for(int j = 0; j < S2; j++) {
+                array[i][j] = (int)(Math.random() * 10);
+            }
+        }        
+        System.out.println("Исходная матрица: ");
+        showMatrix(array);
+        System.out.print("Укажите 2 столбца, которые нужно поменять местами (от 1 до " + S2 + " через пробел): ");
+        Scanner sc = new Scanner(System.in);
+        int t, v, temp;
+        t = sc.nextInt() - 1;
+        v = sc.nextInt() - 1;
+        for(int i = 0; i < S1; i++) {
+            temp = array[i][t];
+            array[i][t] = array[i][v];
+            array[i][v] = temp;
+        }       
+        System.out.println("Результирующая матрица: ");
+        showMatrix(array);   
+    }
+    static void showMatrix(int[][] matrix) {
+        for(int x[] : matrix) {
+            for(int y : x) {
+                System.out.print(y + " ");
+            }
+            System.out.println();
+        }   
+    }
+}
