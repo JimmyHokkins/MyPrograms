c4c59e2593fd6b68fdd77524ae8fa3b0181408c5
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2019-08-06 20:48:46 +0300

    Create Task1.2.java

---
+++ 3_Strings_and_basics_of_text_processing/string_as_object/Task1.2.java
@@ -0,0 +1,21 @@
+public class Task1 {    
+    public static void main(String[] args) {
+        String sourceString = "ghgh   kjjhg   uuuo     iiuu mjjh";
+        System.out.println("Исходная строка: " + sourceString);
+        int max = 0;
+        int count = 0;
+        int fromIndex = 0;        
+        while(fromIndex != -1) {            
+            fromIndex = sourceString.indexOf(' ', fromIndex);
+            if(fromIndex != -1) {
+                while(fromIndex < sourceString.length() && sourceString.charAt(fromIndex) == ' ') {
+                    fromIndex++;
+                    count++;
+                }
+            }
+            if(count > max) max = count;
+            count = 0;
+        }         
+        System.out.println("Максимальное количество пробелов равно: " + max);
+    }    
+}
