fe15360372facb4dfbfeffb1662456817e0fe6b9
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2019-08-06 19:18:49 +0300

    Create Task5.java

---
+++ 3_Strings_and_basics_of_text_processing/string_as_an_array_of_characters/Task5.java
@@ -0,0 +1,20 @@
+public class Task5 {    
+    public static void main(String[] args) {
+        String sourceString = "___kghf_____ffgg__dfreew_____loov_wer___"; // пробелы заменены на подчеркивания для наглядности
+        String resultString = "";
+        System.out.println("Исходная строка:  " + sourceString);        
+        
+        String symb;
+        for(int i = 0; i < sourceString.length(); i++) {
+            if(sourceString.charAt(i) == '_'){               
+                int j = i;
+                while(i < sourceString.length() && sourceString.charAt(i) == '_') i++;
+                if(i == sourceString.length() || j == 0) symb = "";
+                else symb = "_";
+                resultString += symb;                
+            }
+            if(i < sourceString.length()) resultString += sourceString.charAt(i);
+        }
+        System.out.println("Результирующая строка:" + resultString);
+    }    
+}
