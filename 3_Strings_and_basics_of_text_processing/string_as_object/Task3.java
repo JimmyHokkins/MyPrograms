4518c040291b676983306621417983658f57b742
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2019-08-08 20:18:22 +0300

    Create Task3.java

---
+++ 3_Strings_and_basics_of_text_processing/string_as_object/Task3.java
@@ -0,0 +1,18 @@
+/*
+  Условие задачи:
+  Проверить, является ли заданное слово палиндромом
+*/
+public class Task3 {    
+    public static void main(String[] args) { 
+        System.out.print("Введите строку для проверки на палиндром: ");
+        Scanner sc = new Scanner(System.in, "cp1251");
+        String s = sc.nextLine();
+        s = s.toLowerCase();  // приводим строку к нижнему регистру 
+        StringBuilder s2 = new StringBuilder(s);  // создаем временный объект StringBuilder для использования его метода реверсирования строки
+        String s3 = new String(s2.reverse());  // создаем реверсированную строку, используя метод reverse() объекта StringBuilder
+        boolean check = false;
+        if(s.equals(s3)) check = true;  // если обычная и реверсированная строки равны, значит слово палиндром
+        String answer = check ? "слово является палиндромом" : "слово не является палиндромом";
+        System.out.println("Результат проверки: " + answer);
+    }    
+}
