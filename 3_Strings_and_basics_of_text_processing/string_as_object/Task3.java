be64077e7c72f25a8334de71d9b90c94b0a599cd
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2019-08-08 20:45:27 +0300

    Update Task3.java

--- 3_Strings_and_basics_of_text_processing/string_as_object/Task3.java
+++ 3_Strings_and_basics_of_text_processing/string_as_object/Task3.java
@@ -10,7 +10,7 @@ public class Task3 {
         StringBuilder s2 = new StringBuilder(s);  // создаем временный объект StringBuilder для использования его метода реверсирования строки
         String s3 = new String(s2.reverse());  // создаем реверсированную строку, используя метод reverse() объекта StringBuilder
         boolean check = false;
-        if(s.compareToIgnoreCase(s3) == 0) check = true;  // если обычная и реверсированная строки равны, значит слово палиндром
+        if(s.equalsIgnoreCase(s3)) check = true;  // если обычная и реверсированная строки равны, значит слово палиндром
         String answer = check ? "слово является палиндромом" : "слово не является палиндромом";
         System.out.println("Результат проверки: " + answer);
     }    
