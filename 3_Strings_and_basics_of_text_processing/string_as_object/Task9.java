fc56416d679165b2e71d6bbe4d483e430557d2ff
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2020-01-14 21:38:38 +0300

    Create Task9.java

---
+++ 3_Strings_and_basics_of_text_processing/string_as_object/Task9.java
@@ -0,0 +1,32 @@
+/*
+  Посчитать количество строчных и прописных букв в введенной строке. Учитывать только английские буквы.
+*/
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Введите строку с буквами в разном регистре: ");
+        Scanner sc = new Scanner(System.in);
+        String sourceString = sc.nextLine();
+        int countLowerCase = 0;  // маленькие буквы
+        int countUpperCase = 0;  // большие буквы
+        int countOther = 0;  // другие символы
+        char[] letters = sourceString.toCharArray();
+        for(int i = 0; i < letters.length; i++) {
+            if((letters[i] >= 65 && letters[i] <= 90) || (letters[i] >= 97 && letters[i] <= 122)) {
+                if(Character.isUpperCase(letters[i])) {
+                    countUpperCase++;
+                }
+                else if(Character.isLowerCase(letters[i])){
+                    countLowerCase++;
+                }
+            }
+            else {
+                countOther++;
+            }
+        }
+        System.out.println("Число строчных букв: " + countLowerCase + ", число прописных букв: " + countUpperCase +
+                ", число прочих символов: " + countOther);
+    }
+}
