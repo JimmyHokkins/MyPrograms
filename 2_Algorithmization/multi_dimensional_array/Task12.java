ff9d70f13911125aed34cb5f14b2c16e00c2ac87
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2020-05-17 15:53:14 +0300

    Update Task12.java

--- 2_Algorithmization/multi_dimensional_array/Task12.java
+++ 2_Algorithmization/multi_dimensional_array/Task12.java
@@ -1,3 +1,7 @@
+/*
+Отсортировать строки матрицы по возрастанию и убыванию значений элементов.
+*/
+
 public class Task12 {    
     public static void main(String[] args) {
         int S1 = 6, S2 = 10;
