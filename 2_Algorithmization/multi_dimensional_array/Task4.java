2ce843a0a24151a4c956909bd0e49b4505c9ce2d
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2020-05-17 15:33:51 +0300

    Update Task4.java

--- 2_Algorithmization/multi_dimensional_array/Task4.java
+++ 2_Algorithmization/multi_dimensional_array/Task4.java
@@ -1,3 +1,16 @@
+/*
+Сформировать квадратную матрицу порядка n по заданному образцу (n - четное):
+1     2     3  ...  n
+n    n-1   n-2 ...  1
+1     2     3  ...  n
+n    n-1   n-2 ...  1
+.
+.
+.
+n    n-1   n-2 ...  1
+
+*/
+
 public class Task4 {    
     public static void main(String[] args) {
         int S = 6;
