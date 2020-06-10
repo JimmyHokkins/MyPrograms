5a0bb007d3b521520daac22fd662e03c8e24e57f
JimmyHokkins <52209840+JimmyHokkins@users.noreply.github.com>
2020-06-10 21:29:01 +0300

    Update Task1.java

--- 3_Strings_and_basics_of_text_processing/string_as_an_array_of_characters/Task1.java
+++ 3_Strings_and_basics_of_text_processing/string_as_an_array_of_characters/Task1.java
@@ -1,3 +1,7 @@
+/*
+Дан массив названий переменных в camelCase. Преобразовать названия в snake_case.
+*/
+
 public class Task1 {    
     public static void main(String[] args) {
         String[] namesVars = { "thisBeautifulDay", "theBookOfMagia", "piratesSevenSeas" };
